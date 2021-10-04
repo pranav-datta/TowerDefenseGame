9a6e19ca8879635fa90ce7ab0c06d48dc15fb94f
Eric Shavkin <erics@lawn-128-61-5-58.lawn.gatech.edu>
2021-10-04 12:44:28 -0400

    Added Javadocs to initial game screen

--- src/welcomeScreen/InitialGameScreen.java
+++ src/welcomeScreen/InitialGameScreen.java
@@ -10,13 +10,24 @@ public class InitialGameScreen {
     private Controller controller;
     private Stage mainStage;
 
+    /**
+     * No-arg constructor for initial game screen
+     */
     public InitialGameScreen() {
     }
 
+    /**
+     * Constructor for initial game screen
+     * @param controller
+     */
     public InitialGameScreen(Controller controller) {
         this.controller = controller;
     }
 
+    /**
+     * Method where game UI is displayed and user interacts with game application
+     * @param stage stage on which GUI is displayed
+     */
     public void playGame(Stage stage) {
         mainStage = stage;
         mainStage.setTitle("*insert game name here*");
